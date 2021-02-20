b253624353b54f5b9dc1882cbd892dd76382764e
Creandola <comercial@somoscreandola.com>
2021-02-19 21:05:33 -0500

    create migrations app

---
+++ database/migrations/2021_02_20_020143_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->longText('short_text');
+            $table->longText('description');
+            $table->string('image', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
